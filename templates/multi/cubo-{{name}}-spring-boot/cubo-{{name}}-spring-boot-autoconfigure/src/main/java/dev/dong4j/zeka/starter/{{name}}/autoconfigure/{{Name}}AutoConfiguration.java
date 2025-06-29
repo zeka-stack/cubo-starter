ee7bc9e9402829087f8227d1992a02bde7e558b4
dong4j <dong4j@gmail.com>
2025-06-29 16:55:52 +0800

    refactor(cubo-starter): 更新配置属性以启用或禁用 Zeka 功能
    - 将 ZekaProperties.ENABLE 更改为 ZekaProperties.ENABLED

--- templates/multi/cubo-{{name}}-spring-boot/cubo-{{name}}-spring-boot-autoconfigure/src/main/java/dev/dong4j/zeka/starter/{{name}}/autoconfigure/{{Name}}AutoConfiguration.java
+++ templates/multi/cubo-{{name}}-spring-boot/cubo-{{name}}-spring-boot-autoconfigure/src/main/java/dev/dong4j/zeka/starter/{{name}}/autoconfigure/{{Name}}AutoConfiguration.java
@@ -20,7 +20,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnProperty(
     prefix = {{Name}}Properties.PREFIX,
-    name = ZekaProperties.ENABLE,
+    name = ZekaProperties.ENABLED,
     havingValue = ZekaProperties.ON,
     matchIfMissing = true)
 @EnableConfigurationProperties({{Name}}Properties.class)
